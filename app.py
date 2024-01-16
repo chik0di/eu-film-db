194d70e71ebaeb142a129d301302d9ecf98ede0c
chik0di <chikodiobu@gmail.com>
2024-01-17 00:27:01 +0100

    config parsing done directly from app.py

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 # app.py
 import streamlit as st
-from db_utils import get_connection
+import mysql.connector
+import configparser
+
+config_parser = configparser.ConfigParser()
+config_parser.read('config.ini')
+
+def get_connection():
+    config = {
+    'host': config_parser.get('database', 'host'),
+    'user': config_parser.get('database', 'user'),
+    'password': config_parser.get('database', 'password'),
+    'database': config_parser.get('database', 'schema'),
+}
+    try:
+        print("Before connection attempt")
+        connection = mysql.connector.connect(**config)
+        print("Connection successful")
+        return connection
+
+    except Exception as e:
+        print(f"Error connecting to the database: {e}")
+        return None
 
 def fetch_data():
     try:
